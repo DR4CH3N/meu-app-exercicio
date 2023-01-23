5091ced7c1e4323dea54743820d632e4dad0f41e
Matheus Anthony <thehellc4t@gmail.com>
2023-01-23 11:51:06 -0300

    layout basico do exercicio 1

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View, TextInput } from "react-native";
+
+import Localizacao from "./src/components/Localizacao";
+
+import SeletorDeimagem from "./src/components/SeletorDeimagem";
+
+import VisualizadorDeMapa from "./src/components/VisualizadorDeMapa";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>App 1 - foto de lugares visitados</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do local"
+        keyboardType="default"
+      />
+      <Button
+        style={styles.botaoImagem}
+        title="pegue uma imagem do rolo da camera"
+        onPress={SeletorDeimagem}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +29,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  botaoImagem: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
