b1649586db25ed3ba80e1e73c18384d819b156c6
Matheus Anthony <thehellc4t@gmail.com>
2023-01-24 11:08:26 -0300

    adicionado mapa e marcação de ponto (hora e data)

--- App.js
+++ App.js
@@ -3,146 +3,84 @@ import {
   StyleSheet,
   Text,
   View,
-  TextInput,
-  Pressable,
-  SafeAreaView,
   ScrollView,
-  Image,
+  SafeAreaView,
+  Alert,
+  Pressable,
 } from "react-native";
-import * as ImagePicker from "expo-image-picker";
-import * as Location from "expo-location";
-import MapView, { Marker } from "react-native-maps";
 
-export default function ImagePickerExample() {
-  /* ===================================== MAPA E LOCALIZAÇÃO =========================================== */
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
+
+export default function () {
+  var dia = new Date().getDate();
+  var mes = new Date().getMonth() + 1;
+  var ano = new Date().getFullYear();
+  var hora = new Date().getHours();
+  var min = new Date().getMinutes();
+  var sec = new Date().getSeconds();
 
   const [minhaLocalizacao, setMinhaLocalizacao] = useState(null);
 
   useEffect(() => {
     async function obterLocalizacao() {
-      /* Acessandp o status da requisição de permissão de uso */
       const { status } = await Location.requestForegroundPermissionsAsync();
 
-      /* Acessando os dados de geolocalização */
       let localizacaoAtual = await Location.getCurrentPositionAsync({});
 
-      /* Adicionando os dados ao state */
-      setMinhaLocalizacao(localizacaoAtual);
+      setMinhaLocalizacao();
     }
-
-    obterLocalizacao();
   }, []);
 
-  // console.log(minhaLocalizacao);
+  console.log(minhaLocalizacao);
 
   const regiaoInicial = {
     // Estado de SP
-    latitude: -23.533773,
-    longitude: -46.65529,
-    latitudeDelta: 10,
-    longitudeDelta: 10,
-  };
-
-  /* Usando state para controlar a localização */
-  const [localizacaoClicada, setLocalizacaoClicada] = useState();
-
-  const marcarLocal = (event) => {
-    setLocalizacaoClicada({
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-      latitude: minhaLocalizacao.coords.latitude,
-      longitude: minhaLocalizacao.coords.longitude,
-    });
-
-    setTimeout(() => {
-      console.log(localizacaoClicada);
-    }, 3000);
-  };
-
-  /* ===================================== FOTO =========================================== */
-
-  const [foto, setFoto] = useState(); // Recupera a foto que vc vai bater com a câmera
-
-  const acessarCamera = async () => {
-    const imagem = await ImagePicker.launchCameraAsync({
-      allowsEditing: true,
-      aspect: [16, 9],
-      quality: 0.5,
-    });
-
-    console.log(imagem);
-
-    setFoto(imagem.assets[0].uri);
+    minhaLocalizacao,
   };
 
   return (
     <>
       <ScrollView contentContainerStyle={estilos.contentContainer}>
         <SafeAreaView style={estilos.safeContainer}>
-          <Text style={estilos.tituloExercicio}>
-            App 1 - Fotos de lugares visitados
+          <Text style={estilos.titulo}>App 2 - Marcação de Ponto</Text>
+          <View style={estilos.mapa}>
+            <MapView
+              style={{ width: "100%", height: 200 }}
+              liteMode={false}
+              mapType="standard"
+              userInterfaceStyle="dark"
+            ></MapView>
+          </View>
+
+          <Text style={estilos.data}>
+            {" "}
+            {hora +
+              ":" +
+              min +
+              " " +
+              "-" +
+              " " +
+              dia +
+              "/" +
+              mes +
+              "/" +
+              ano}{" "}
           </Text>
-
-          <View style={estilos.containerNomeLocal}>
-            <TextInput
-              style={estilos.campoLocal}
-              placeholder="Titulo da foto/local"
-            />
-          </View>
-
-          <View style={estilos.foto}>
-            {foto && (
-              <Image
-                source={{ uri: foto }}
-                style={{ width: 371, height: 200 }}
-              />
-            )}
-          </View>
 
           <Pressable
             style={({ pressed }) => [
               {
                 backgroundColor: pressed ? "gray" : "#f4f4f4",
               },
-              estilos.botaoFoto,
+              estilos.botaoPonto,
             ]}
-            onPress={acessarCamera}
+            onPress={() => {
+              Alert.alert("Registro", "Ponto registrado com sucesso!");
+            }}
           >
-            <Text style={estilos.textoBotaoFoto}>Tirar Foto</Text>
+            <Text style={estilos.textoBotao}>Marcar ponto</Text>
           </Pressable>
-
-          <View style={estilos.mapa}>
-            <MapView
-              style={{ height: 200 }}
-              region={localizacaoClicada ?? regiaoInicial}
-              liteMode={false}
-              mapType="standard"
-              userInterfaceStyle="dark"
-              onPress={marcarLocal}
-            >
-              {localizacaoClicada && (
-                <Marker
-                  coordinate={localizacaoClicada}
-                  title="Sua localização!!!"
-                  onPress={(e) => console.log(e.nativeEvent)}
-                />
-              )}
-            </MapView>
-          </View>
-
-          {minhaLocalizacao && (
-            <Pressable
-              style={({ pressed }) => [
-                {
-                  backgroundColor: pressed ? "gray" : "#f4f4f4",
-                },
-                estilos.botaoLocalizar,
-              ]}
-              onPress={marcarLocal}
-            >
-              <Text style={estilos.textoLocalizar}>Salvar localização</Text>
-            </Pressable>
-          )}
         </SafeAreaView>
       </ScrollView>
     </>
@@ -151,66 +89,40 @@ export default function ImagePickerExample() {
 
 const estilos = StyleSheet.create({
   contentContainer: {
+    flex: 1,
     paddingVertical: 1,
-    // flex: 1,
   },
   safeContainer: {
-    // flex: 1,
+    flex: 1,
     backgroundColor: "#f4f4f4",
-    padding: 8,
   },
-  tituloExercicio: {
+  titulo: {
     fontSize: 20,
     textAlign: "center",
-    marginTop: 50,
-  },
-  campoLocal: {
-    borderWidth: 1,
-    fontSize: 18,
-    marginTop: 25,
-    height: 54,
-    width: "90%",
-    textAlign: "center", // Deixa o placheholder centralizado
-  },
-  containerNomeLocal: {
-    alignItems: "center",
-  },
-  foto: {
-    marginTop: 20,
-    width: "80%",
-    height: 200,
-    backgroundColor: "lightblue",
-    justifyContent: "center",
-    // marginLeft: 20,
-  },
-  botaoFoto: {
-    marginTop: 20,
-    borderWidth: 1,
-    height: 54,
-    width: "90%",
-    marginLeft: 20,
-  },
-  textoBotaoFoto: {
-    fontSize: 20,
-    alignSelf: "center",
-    marginTop: 10,
+    marginTop: 80,
   },
   mapa: {
-    marginTop: 20,
-    width: "90%",
+    // bloco em sobre o mapa
+    marginTop: 50,
     height: 200,
-    backgroundColor: "lightblue",
+    width: "100%",
+    backgroundColor: "#d3d3d3",
     justifyContent: "center",
-    marginLeft: 20,
+    borderRadius: 10,
   },
-  botaoLocalizar: {
+  data: {
+    fontSize: 20,
+    textAlign: "center",
+    marginTop: "5%",
+  },
+  botaoPonto: {
     marginTop: 20,
     borderWidth: 1,
     height: 54,
     width: "90%",
     marginLeft: 20,
   },
-  textoLocalizar: {
+  textoBotao: {
     fontSize: 20,
     alignSelf: "center",
     marginTop: 10,
